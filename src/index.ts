806d0eaca3132759feb8f4baa4cb6b25ccc69f39
Harsh Kumar <harshkumar.baldaniya@ei.study>
2024-03-09 21:23:03 +0530

    - Add first request to go to the textbook home page

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
 import express from "express";
-import { createDatabaseConnection } from "./database.js";
+import { dbConnection } from "./config/database.js";
+import router from "./routes/index.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
-console.log(createDatabaseConnection);
+dbConnection().then(() => {
+    console.log('Database connection successfully established');
 
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Server start on ${PORT}`);
+    app.listen(PORT, () => {
+        console.log(`schoolSite service is running on ${PORT}`);
+    })
+
+    app.use(router);
+}).catch(err => {
+    console.error('Database connection failed:', err);
+    throw err;
 })
